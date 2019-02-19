960ed057f6abf4d960f9cd549984a7aee91704eb
Matheson Steplock <ikifar2012@users.noreply.github.com>
2019-02-18 19:25:56 -0500

    cancel script timer

--- www/deb.sh
+++ www/deb.sh
@@ -6,5 +6,25 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 echo rebooting 
 echo 10 seconds to cancel 
-sleep 10
+sleep 1
+echo 9 seconds to cancel 
+sleep 1
+echo 8 seconds to cancel 
+sleep 1
+echo 7 seconds to cancel 
+sleep 1
+echo 6 seconds to cancel 
+sleep 1
+echo 5 seconds to cancel 
+sleep 1
+echo 4 seconds to cancel 
+sleep 1
+echo 3 seconds to cancel 
+sleep 1
+echo 2 seconds to cancel 
+sleep 1
+echo 1 seconds to cancel 
+sleep 1
+echo rebooting
+echo goodbye
 sudo reboot
