0e3c450543bfd4a36888a82b2915a9166d31e305
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-05-11 16:30:57 -0400

    update

--- www/deb-auto.sh
+++ www/deb-auto.sh
@@ -1,7 +1,10 @@
 date="date -I"
+backupdir="$PWD/backup/$date"
 read -p "Would you like to backup your current configuration? [Y/n]" answer
 
-if [[$answer]]
-
-mkdir -p $PWD/backup/$date
-echo "Backing up old files to $PWD/backup/$date"
+if [ $answer -eq "Y" ]
+then 
+mkdir -p $backupdir
+echo "Backing up old files to $backupdir"
+else
+echo "Your files are being overwritten"
