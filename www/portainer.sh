1a2d51034977f9488b250ec684ffbdf2a3917d3e
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-05-24 19:04:24 -0400

    portainer

---
+++ www/portainer.sh
@@ -0,0 +1,6 @@
+docker stop portainer
+docker stop Portainer
+docker rm Portainer
+docker rm portainer
+docker pull portainer/portainer
+docker run -d -p 9000:9000 --restart always --name Portainer -v /opt/portainer-data:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
