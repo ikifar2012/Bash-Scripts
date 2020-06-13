67c616c1111314a4646646ffe27d78cb3b710a1e
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-06-13 17:25:31 -0400

    deb-auto

--- www/deb-auto.sh
+++ www/deb-auto.sh
@@ -7,6 +7,9 @@ if [[ $userin = "y" ]]
 then 
 mkdir -p $backupdir
 echo "Backing up old files to $backupdir"
+cp -p /etc/apt/apt.conf.d/50unattended-upgrades $backupdir
+cp -p /etc/apt/apt.conf.d/20auto-upgrades $backupdir
+echo "Files backed up"
 else
 echo "Your files are being overwritten"
 fi
