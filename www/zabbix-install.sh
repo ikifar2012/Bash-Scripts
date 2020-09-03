cb78f8a3b1be9ca51dac75811243d503c86f43bf
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-09-02 23:58:18 -0400

    zabbix

--- www/zabbix-install.sh
+++ www/zabbix-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
-sudo dpkg -i zabbix-release_5.0-1+focal_all.deb
+sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb -O /tmp/zabbix-release_5.0-1+focal_all.deb
+sudo dpkg -i /tmp/zabbix-release_5.0-1+focal_all.deb
 sudo apt update
 sudo apt install -y zabbix-agent
