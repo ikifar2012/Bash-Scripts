aab8e88e9bda1ab68e4b4697bf29e15b2c951c1e
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-09-05 20:55:33 -0400

    mv agent

--- www/zabbix-install.sh
+++ www/zabbix-install.sh
@@ -4,11 +4,12 @@ sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/z
 sudo dpkg -i /tmp/zabbix-install/zabbix-release_5.0-1+focal_all.deb
 sudo apt update
 sudo apt install -y zabbix-agent
-sudo wget https://raw.githubusercontent.com/ikifar2012/Bash-Scripts/master/www/files/zabbix-install/zabbix_agentd.conf -O /tmp/zabbix-install/zabbix_agentd.conf
+sudo wget https://raw.githubusercontent.com/ikifar2012/Bash-Scripts/master/www/files/zabbix-install/zabbix_agentd.conf -O /tmp/zabbix-install/zabbix_agentd.conf.tmp
 export HOSTNAME=$(hostname)
 read -p "Enter Zabbix Server IP:" ZABBIXSERVER
 export ZABBIXSERVER=$ZABBIXSERVER
-sudo cat /tmp/zabbix-install/zabbix_agentd.conf | envsubst > /etc/zabbix/zabbix_agentd.conf
+cat /tmp/zabbix-install/zabbix_agentd.conf.tmp | envsubst > /tmp/zabbix-install/zabbix_agentd.conf
+sudo mv /tmp/zabbix-install/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
 sudo systemctl stop zabbix-agent
 sudo systemctl start zabbix-agent
 sudo systemctl enable zabbix-agent
