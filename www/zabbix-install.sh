1e400ddf21b1b79c5f4e572c438dabc3d201635a
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-09-05 20:30:03 -0400

    zabbix

--- www/zabbix-install.sh
+++ www/zabbix-install.sh
@@ -3,9 +3,11 @@ sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/z
 sudo dpkg -i /tmp/zabbix-install/zabbix-release_5.0-1+focal_all.deb
 sudo apt update
 sudo apt install -y zabbix-agent
-
-sudo systemctl enable zabbix-agent
+sudo wget https://raw.githubusercontent.com/ikifar2012/Bash-Scripts/master/www/files/zabbix-install/zabbix_agentd.conf -O /tmp/zabbix-install/zabbix_agentd.conf
 export HOSTNAME=$(hostname)
 read -p "Enter Zabbix Server IP:" ZABBIXSERVER
 export ZABBIXSERVER=$ZABBIXSERVER
-cat /etc/zabbix/zabbix_agentd.conf | envsubst 
+cat /tmp/zabbix-install/zabbix_agentd.conf | envsubst > /etc/zabbix/zabbix_agentd.conf
+sudo systemctl start zabbix-agent
+sudo systemctl enable zabbix-agent
+echo "Zabbix agent running with hostname: $HOSTNAME"
