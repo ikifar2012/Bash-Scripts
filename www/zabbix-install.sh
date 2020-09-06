8c7f8690c40581bff733755a1d573c60d5c66811
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-09-05 20:24:43 -0400

    zabbix conf

--- www/zabbix-install.sh
+++ www/zabbix-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb -O /tmp/zabbix-release_5.0-1+focal_all.deb
-sudo dpkg -i /tmp/zabbix-release_5.0-1+focal_all.deb
+sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb -O /tmp/zabbix-install/zabbix-release_5.0-1+focal_all.deb
+sudo dpkg -i /tmp/zabbix-install/zabbix-release_5.0-1+focal_all.deb
 sudo apt update
 sudo apt install -y zabbix-agent
 
 sudo systemctl enable zabbix-agent
+export HOSTNAME=$(hostname)
+read -p "Enter Zabbix Server IP:" ZABBIXSERVER
+export ZABBIXSERVER=$ZABBIXSERVER
+cat /etc/zabbix/zabbix_agentd.conf | envsubst 
