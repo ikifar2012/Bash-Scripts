47b31fbf762bebb42cf76e36d268f933d8efe602
Matheson Steplock <ikifar2012@users.noreply.github.com>
2021-04-13 23:27:34 -0400

    update to portainer-ce

--- www/portainer.sh
+++ www/portainer.sh
@@ -2,5 +2,5 @@ docker stop portainer
 docker stop Portainer
 docker rm Portainer
 docker rm portainer
-docker pull portainer/portainer
+docker pull portainer/portainer-ce
 docker run -d -p 9000:9000 --restart always --name Portainer -v /opt/portainer-data:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
